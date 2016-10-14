73129a416a3688e1e27156d2279d99305b5848c6
Dylan <pajorr2@gmail.com>
2016-10-14 08:02:20 +0700

    Update 1

---
+++ Beepiano.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <windows.h> // to clear the console when needed using system("cls")
+
+// Main plan : To assign notes on keyboard keys, and using if to execute these notes. Notes will be played automatically when pressing the keys. (No need to press enter.)
+
+using namespace std;
+
+int main()
